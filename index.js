81ea8fcdce41fe904f0384032378a342171f188c
farhangalib-1 <farhan.galib_cumilla1@yahoo.com>
2026-06-18 01:58:16 +0600

    MongoDB is added

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+app.use(cors());
+app.use(express.json());
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    
+    await client.connect();
+   const db = client.db("idea-vault-backend")
+   const collections = db.collection("collections");
+   app.get("/allcollections", async(req, res)=>{
+    const cursor = await collections.find();
+    const result = await cursor.toArray();
+    res.send(result);
+   })
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    
+  }
+}
+run().catch(console.dir);
+app.get("/", (req, res)=>{
+    res.send("Hello World");
+})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
